ba972ba21be09b138cb20de23307b1584a7125e7
akmalryandi <akmalryandii@gmail.com>
2023-11-07 19:27:26 +0700

    [FEAT] New Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
 Route::get('/', function () {
-    return view('helloword');
+    return view('welcome');
 });
+
+Route::get('/biodata', [HomeController::class, 'index']);
